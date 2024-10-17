085b89198c3e6ee3b382fecdaf755a6987b43d45
Juan Sebastian Cadena Ramirez <juan.cadena.ramirez@udla.edu.ec>
2024-10-17 16:17:03 -0500

    primer cambio

---
+++ PCommit/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using PCommit.Models;
+using System.Diagnostics;
+
+namespace PCommit.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        /*
+         
+         Prueba Commit Git 33
+         
+         */
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
